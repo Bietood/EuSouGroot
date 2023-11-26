fb5be4d0484940c2ba2dee1f1009154f02bc6046
Bietood <gabriel.duarte69@etec.sp.gov.br>
2023-11-26 18:34:16 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿const string sair = "tchau";
+string pergunta;
+
+Console.Clear();
+
+Console.WriteLine(" Conversa com o Groot  ");
+Console.WriteLine();
+
+do
+{
+    Console.Write("Digite sua pergunta: ");
+    pergunta = Console.ReadLine()!;
+    
+    Console.WriteLine($"                          Groot: Eu sou Groot ");
+}while (pergunta != sair);
